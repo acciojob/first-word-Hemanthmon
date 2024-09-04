5e165440db6e960d13a490f9b15d36d54c17d43d
Hemanthmon <153289923+Hemanthmon@users.noreply.github.com>
2024-09-04 18:03:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function firstWord(s) {
-  // your code here
+   let word = s.split(' '); // this will create an array where each word is an element
+	return word[0]; //this will return the first word
+	
+	
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
